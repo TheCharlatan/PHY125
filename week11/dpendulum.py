05915413faf7ee74e51cb2629a50d3a23ca066b6
TheCharlatan <seb.kung@gmail.com>
2016-05-20 16:24:41 +0200

    Add Animation on Double Pendulum

--- week11/dpendulum.py
+++ week11/dpendulum.py
@@ -4,56 +4,90 @@ import numpy as np
 import matplotlib.pyplot as pl
 import matplotlib.animation as animation
 
+class Pendulum:
+  time = 0.0
+  X = 0.0
+  Y = 0.0
+  x = 0.0
+  y = 0.0
+  b = 0.0
+  l = 1.0
+  omega = 0.0
+  dt = 0.0
+  e = 0.0
+  q = np.array([0,0])
+  u = 0
+  g = 9.81
+
+  def __init__(self, b=1, l=1, omega=1.0):
+    self.X = 0
+    self.Y = -b
+    self.x = 0
+    self.y = -(b+l)
+    self.b = b
+    self.l = l
+    self.omega = omega
+    self.dt = self.omega/50.0
+    self.e = (self.b/self.l) * omega**2
+
+  def step(self):
+    self.time += self.dt
+    self.X,self.Y = self.Motor()
+    self.x,self.y = self.Free()
+
+  def Motor(self):
+    return self.b*np.sin(self.omega*self.time), -self.b*np.cos(omega*self.time)
+
+  def Free(self):
+    self.runge2()
+    return self.X + self.l * np.sin(self.q[0]), self.Y + self.l * -np.cos(self.q[0])
+
+  def runge2(self):
+    """Solves a differential equation with Runge-Kutta 2
+    F: function to be solved for
+    ini: array
+        holds the starting requirements and is updated in every iteration
+    t: linearly spaced array
+        holds the time for the different points to be draw"""
+
+    #delta_t = t[-1] - t[-2]
+    #values = []
+    #for time in t:
+    self.q = self.q + 1/2 * self.dt * (self.F(self.q,self.time) + self.F(self.q + self.dt * self.F(self.q,self.time), self.time+self.dt))
+
+
+  def F(self,q,t):
+    #"""The differential function in a four dimensional vector, first two dimensions for space, latter for velocity into given spatial dimension"""
+    #return np.array([X[2],X[3],2*X[3],-2*X[2]+np.cos(omega * t)])
+    return np.array([q[1], -np.sin(self.q[0]) - self.e * np.sin(self.q[0] - self.omega*t)])
+
 #Angular frequency of the driving rod
-omega = 1
+omega = 2
 #Length of the driving rod between its points of fixture and hinge with the free swinging point
 b = 1
+#Length of the flexible dysonant rod
+l = 1
+#Starting coordinates of the harmonic rod
 
-def runge_2(F, ini, t):
-  """Solves a differential equation with Runge-Kutta 2
-  F: function to be solved for
-  ini: array
-      holds the starting requirements and is updated in every iteration
-  t: linearly spaced array
-      holds the time for the different points to be draw"""
-
-  delta_t = t[-1] - t[-2]
-  values = []
-  for time in t:
-    ini = ini + 1/2 * delta_t * (F(ini,time) + F(ini + delta_t * F(ini,time), time))
-    values.append(1*ini)
-  return values
-
-
-def F(X,t):
-  """The differential function in a four dimensional vector, first two dimensions for space, latter for velocity into given spatial dimension"""
-  return np.array([X[2],X[3],2*X[3],-2*X[2]+np.cos(omega * t)])
-
-
-def Motor(b,omega,t):
-  return b*np.sin(omega*t), -b*cos(omega*t)
-
-def run()
-
-#Starting position
-X = 0
-Y = b
+pendel = Pendulum(b, l, omega)
 
 fig = pl.figure()
-ax = fig.add_subplot(111, autoscale_on=False)
+ax = fig.add_subplot(111, xlim=(-(b+l),b+l), ylim=(-(b+l),b+l))
 ax.grid()
-point, = ax.plot([],[], 'r.')
+Bpoint, = ax.plot([],[], 'r.')
+Lpoint, = ax.plot([],[], 'b.')
+Bline, = ax.plot([],[], 'k-')
+Lline, = ax.plot([],[], 'k-')
 
-def init():
-  point.set_data([x],[y])
-  return point
 
 def animate(i):
-  X,Y = run()
-  point.set_data(x,y)
-  return point
+  pendel.step()
+  Bpoint.set_data([pendel.X,pendel.Y])
+  Bline.set_data([0,pendel.X],[0,pendel.Y])
+  Lpoint.set_data([pendel.x,pendel.y])
+  Lline.set_data([pendel.X,pendel.x],[pendel.Y,pendel.y])
 
-ani = animation.FuncAnimation(fig, animate, interval=25, blit=True, init_func=init)
+ani = animation.FuncAnimation(fig, animate, interval=25)# init_func=init)
 
-
+pl.show()
 
